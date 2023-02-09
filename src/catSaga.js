47d10dbfe10eeddd42ec62935a039a8c94bf485d
matan2288 <60103076+matan2288@users.noreply.github.com>
2023-02-09 09:47:26 +0200

    Update catSaga.js

--- src/catSaga.js
+++ src/catSaga.js
@@ -5,7 +5,7 @@ import { getCatsSuccess } from './catState';
 // fork > is useful when a saga needs to start a non-blocking task. Non-blocking here means: the caller starts the task and continues executing without waiting for it to complete.
 // call > call our url's and api's
 // put > call our actions(dispatch our action)
-// take > wait for an action, an event to occur, and take as argument a string that is not a function of the action, but an action.type(e.g. GET_USERS_SUCCESS action.type).
+// take > wait for an action, an event to occur, and take as argument a string that is not a function of the action, but an action.type(e.g. take(GET_USERS_SUCCESS) action.type).
 // takeEvery > watch our funtion/action and trigger a function whenever an action/function being called (e.g. takeEvery(action.type, generatorFucntionToRun)
 // yield > being used to handle async actions, we can look at it as async await
 
